72fd26dfe3d8342ada097b17634e7eb87c517b3b
Paulo Affonso <plpaulo25affonso@outlook.com.br>
2022-08-01 23:58:05 -0300

    atualizando

--- php/listarLivro.php
+++ php/listarLivro.php
@@ -5,21 +5,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;900&display=swap" rel="stylesheet">
     <link href="../css/reset.css" rel="stylesheet">
     <link href="../css/main.css" rel="stylesheet">
     <title>Listagem dos Livros Cadastrados</title>
+    <script src="https://kit.fontawesome.com/2e1125801e.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <header>
         <nav>
-            <h1>Biblioteca da Rebeca</h1>
-            <a href="../html/cadastrarLivro.html">Cadastrar Livro</a>
+            <h1><i class="fa-solid fa-book-open"></i> Biblioteca</h1>
+            <!-- <button><i class="fa-solid fa-plus"></i><a class="cadastro" href="../html/cadastrarLivro.html"> Cadastrar novo livro</a></button> -->
+            <a href="../html/cadastrarLivro.html"><button class="cssbuttons-io-button"> Cadastrar novo livro
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
+                    </svg>
+                </div>
+            </button>
+            </a>
         </nav>
     </header>
     <main>
         <h1>Livros Cadastrados</h1>
-
+        </br>
         <table>
             <thead>
                 <tr>
@@ -30,30 +42,29 @@
                 </tr>
             </thead>
             <tbody>
-            <?php
-                    
-                    require_once 'conexao.php';
-                    $pdo = null;
-                    $livros = [];
+                <!-- <?php
 
-                    try{
+                        require_once 'conexao.php';
+                        $pdo = null;
+                        $livros = [];
 
-                        $pdo = conexaoPDO();
+                        try {
 
-                        $ps = $pdo->prepare(
-                            'SELECT * FROM livros'
-                        );
+                            $pdo = conexaoPDO();
 
-                        $ps->execute();
+                            $ps = $pdo->prepare(
+                                'SELECT * FROM livros'
+                            );
 
-                        $livros = $ps->fetchAll( PDO::FETCH_ASSOC );
+                            $ps->execute();
 
-                    }catch ( PDOException $e){
-                         echo 'Erro ao Connectar', $e->getMessage(), '<br />';
-                    }
+                            $livros = $ps->fetchAll(PDO::FETCH_ASSOC);
+                        } catch (PDOException $e) {
+                            echo 'Erro ao Connectar', $e->getMessage(), '<br />';
+                        }
 
-                    foreach ( $livros as $l ) {
-                        echo <<<HTML
+                        foreach ($livros as $l) {
+                            echo <<<HTML
                             <div class="container-btn">
                                 <tr>
                                     <td>${l['id']}</td>
@@ -65,10 +76,11 @@
                                 </tr>
                             </div>
                         HTML;
-                    }
-                ?>
+                        }
+                        ?> -->
             </tbody>
         </table>
     </main>
 </body>
+
 </html>
